0e0646e4487e1604c0b538ff146f3063da574647
jmzhu3 <jmzhu3@iflytek.com>
2022-12-18 22:27:23 +0800

    update

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.R2rBridgeRenderer = exports.R2rBridgeMain = void 0;
+var rr_main_1 = require("./rr_main");
+Object.defineProperty(exports, "R2rBridgeMain", { enumerable: true, get: function () { return rr_main_1.R2rBridgeMain; } });
+var rr_renderer_1 = require("./rr_renderer");
+Object.defineProperty(exports, "R2rBridgeRenderer", { enumerable: true, get: function () { return rr_renderer_1.R2rBridgeRenderer; } });
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { R2rBridgeMain } from "./rr_main";
+import { R2rBridgeRenderer } from "./rr_renderer";
+export { R2rBridgeMain, R2rBridgeRenderer };
